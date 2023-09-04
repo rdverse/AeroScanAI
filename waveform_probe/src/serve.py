efe22689bdc20203eda49c22aa2eb86ca2f9418e
rdverse <deveshreddy27@gmail.com>
2023-09-03 23:41:00 -0500

    modification to inference

--- waveform_probe/src/serve.py
+++ waveform_probe/src/serve.py
@@ -2,7 +2,7 @@ import uvicorn
 from fastapi import FastAPI
 import logging
 import warnings
-from predict import inference
+from predict import WaveInference
 import pandas as pd
 import inspect
 from fastapi import FastAPI
@@ -50,14 +50,28 @@ async def train(payload:TrainPayload):
     model.save(payload.model_path)
     logger.info("Saved Robotic Maintenance Model")
     accuracy_score = model.validate()
-    return {"msg" : f"{fi} Model trained succesfully model path is {payload.model_path}\n feature importance : {fi}", "validation scores": accuracy_score}
+    return {"msg" : f"{fi} Model trained succesfully model path is {payload.model_path}\n feature importance : {fi}",
+            "validation scores": accuracy_score}
 
 @app.post("/predict")
 async def predict(payload:PredictionPayload):
-    print("entered predict")
-    sample = pd.json_normalize(payload.data)
-    results = inference(data = sample, model_path = payload.model_path, num_class = payload.num_class)
-    return {"msg": "Completed Analysis", "Maintenance Recommendation": results}
+    #sample = pd.json_normalize(payload.data) 
+    inferenceEngine = WaveInference(payload.model_path, payload.data_path)
+    inferenceEngine.load_model_and_data()
+    results, wavetoplot = inferenceEngine.inference(x_coord=payload.x_coord , y_coord=payload.y_coord)
+    feature_importance = inferenceEngine.lime_explanation(x_coord=payload.x_coord , y_coord=payload.y_coord)
+    print("back to predict")
+    print(results)
+    print(wavetoplot)
+    return {"msg": "Completed Analysis", "results": results, "wavetoplot": str(list(wavetoplot[0])), "feature_importance": str(list(feature_importance))}
+
+# @app.post("/plotwaveform")
+# async def plotwaveform(payload:PredictionPayload):
+#     print("entered plotwaveform")
+#     #sample = pd.json_normalize(payload.data)
+#     data = get_data(x_coord=payload.x_coord , y_coord=payload.y_coord)    
+#     return {"msg": "Fetched waveform", "waveform": data}
+
 
 #add implementation for retraining the model 
 if __name__ == "__main__":
