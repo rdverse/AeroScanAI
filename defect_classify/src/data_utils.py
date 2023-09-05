b1f719c5f5a60e9baf8f92e0b5b1e8e82b7cdc3f
rdverse <deveshreddy27@gmail.com>
2023-09-05 11:38:09 -0500

    Made some corrections to the noise levels

--- defect_classify/src/data_utils.py
+++ defect_classify/src/data_utils.py
@@ -1,3 +1,4 @@
+import math
 import random
 import numpy as np
 import pandas as pd
@@ -10,8 +11,8 @@ logger = logging.getLogger(__name__)
 SCAN_DICT = {"low_defect_scan": {"defect_coverage": 0.15, "random_seed": 9}, 
              "medium_defect_scan": {"defect_coverage": 0.5, "random_seed": 9}, 
              "high_defect_scan": {"defect_coverage": 0.8, "random_seed": 9},
-             "random": {"defect_coverage": np.random.randn(), "random_seed": int(np.random.uniform(1,1000))}}
-
+             "random": {"defect_coverage": np.random.randn(), "random_seed": int(np.random.uniform(10,1000))}}
+# go to CHECKPOINT-1 to change level of noise
 def synthetic_defects(img_dim, n_channels, scan_type):
     defect_coverage = SCAN_DICT[scan_type]["defect_coverage"]
     random_seed = SCAN_DICT[scan_type]["random_seed"]
@@ -27,10 +28,21 @@ def synthetic_defects(img_dim, n_channels, scan_type):
         sine_wave = 1 * np.sin(2 * np.pi * frequency * np.arange(length) + phase) +1
         # Add noise
         noise_level = 1
-        noise = np.random.random_sample(noise_level)
+        noise = np.random.uniform(0,noise_level, n_channels)
         if defect:
-            noise = noise + np.random.randint(0, 3, 10)
-        sine_wave += noise
+            # add additional noise to defect signal
+            noise = noise #+ np.random.randint(0, 1, n_channels)
+            sine_wave += noise
+            # dampen the signal at random points
+            dampen_width = math.ceil(n_channels//10)
+            n_dampen_points = np.random.choice([1,2,3])
+            #if dampen_width>0:
+                #for i in range(n_dampen_points):
+                    #dampen_start = np.random.randint(0, n_channels - dampen_width - 1)
+                    #dampen_end = dampen_start + dampen_width
+                    #sine_wave[dampen_start:dampen_end] = 0
+        else:
+            sine_wave += noise
         return sine_wave
     
     def feature_builder(datapoint):
@@ -45,7 +57,7 @@ def synthetic_defects(img_dim, n_channels, scan_type):
         return features
     
     # Calculate the total area of the array
-    data = np.zeros((img_dim, img_dim,10))
+    data = np.zeros((img_dim, img_dim, n_channels))
     mask = np.zeros((img_dim, img_dim))
     pixelmap = np.zeros((img_dim, img_dim, 2)) # 2 for x and y
     defect_area = int(img_dim * defect_coverage)
@@ -81,12 +93,12 @@ def synthetic_defects(img_dim, n_channels, scan_type):
                 else:
                     data[i][j] = random_sine_wave(n_channels, defect=False)
 
-    sine = random_sine_wave(10, defect=True)
+    sine = random_sine_wave(n_channels, defect=True)
     #plt.plot(sine)
     # Plot the array
     #print(np.min(data), np.max(data))
     scale = MinMaxScaler()
-    data = np.round(scale.fit_transform(data.reshape(-1, 10)))#.reshape(64, 64, 10), 3)
+    data = np.round(scale.fit_transform(data.reshape(-1, n_channels)))#.reshape(64, 64, n_channels), 3)
     data = feature_builder(data)
     mask = mask.reshape(-1,).reshape(-1,1)
     pixelmap = pixelmap.reshape(img_dim, img_dim, 2).reshape(-1, 2)
@@ -94,8 +106,9 @@ def synthetic_defects(img_dim, n_channels, scan_type):
     print(np.min(data), np.max(data), np.median(data))
 
     # last column is defect
-    columns = ['min', 'max', 'mean', 'std', 'snr', 'num_peaks', "backwall", "frontwall", "ramp",  "geometry", "defect"]
+    #columns = ['min', 'max', 'mean', 'std', 'snr', 'num_peaks', "backwall", "frontwall", "ramp",  "geometry", "defect"]
     # add zeros where the new artificial features will be
+    columns = ['min', 'max', 'mean', 'std', 'snr', 'num_peaks', "qc1", "qc2", "qc3", "qc4", "defect"]
     data = np.column_stack((data, np.zeros((data.shape[0], 4))))
     data = np.column_stack((data, mask))
     df = pd.DataFrame(data, columns=columns)
@@ -111,19 +124,18 @@ def synthetic_defects(img_dim, n_channels, scan_type):
     #print(df[df['defect'] == 1].shape)
     #print(df[df['defect'] == 2].shape)
     #print(np.random.choice([1, 2], size=(df['defect'] == 1).sum(), p=[0.5, 0.5]))
-
-    
     # backwall
     df.loc[df['defect'] == 1, 'backwall'] = np.random.choice([0, 1], size=(df['defect'] == 1).sum(), p=[0.2, 0.8])
-    df.loc[df['defect'] == 2, 'backwall'] = np.random.choice([0, 1], size=(df['defect'] == 2).sum(), p=[0.8, 0.2])
+    df.loc[df['defect'] == 2, 'backwall'] = np.random.choice([0, 1], size=(df['defect'] == 2).sum(), p=[0.7, 0.3])
     # frontwall
-    df.loc[df['defect'] == 1, 'frontwall'] = np.random.choice([0, 1], size=(df['defect'] == 1).sum(), p=[0.8, 0.2])
-    df.loc[df['defect'] == 2, 'frontwall'] = np.random.choice([0, 1], size=(df['defect'] == 2).sum(), p=[0.2, 0.8])
+    df.loc[df['defect'] == 1, 'frontwall'] = np.random.choice([0, 1], size=(df['defect'] == 1).sum(), p=[0.3, 0.7])
+    df.loc[df['defect'] == 2, 'frontwall'] = np.random.choice([0, 1], size=(df['defect'] == 2).sum(), p=[0.8, 0.2])
     # ramp
-    df.loc[df['defect'] == 1, 'ramp'] = np.random.choice([0, 1], size=(df['defect'] == 1).sum(), p=[0.6, 0.4])
-    df.loc[df['defect'] == 2, 'ramp'] = np.random.choice([0, 1], size=(df['defect'] == 2).sum(), p=[0.4, 0.6])
+    df.loc[df['defect'] == 1, 'ramp'] = np.random.choice([0, 1], size=(df['defect'] == 1).sum(), p=[0.7, 0.3])
+    df.loc[df['defect'] == 2, 'ramp'] = np.random.choice([0, 1], size=(df['defect'] == 2).sum(), p=[0.2, 0.8])
     # geometry
-    df.loc[df['defect'] == 1, 'geometry'] = np.random.choice([0, 1], size=(df['defect'] == 1).sum(), p=[0.4, 0.6])
-    df.loc[df['defect'] == 2, 'geometry'] = np.random.choice([0, 1], size=(df['defect'] == 2).sum(), p=[0.6, 0.4])
-    
-    return df.drop(columns = ["defect"]), df["defect"], pixelmap, columns 
+    df.loc[df['defect'] == 1, 'geometry'] = np.random.choice([0, 1], size=(df['defect'] == 1).sum(), p=[0.8, 0.2])
+    df.loc[df['defect'] == 2, 'geometry'] = np.random.choice([0, 1], size=(df['defect'] == 2).sum(), p=[0.3, 0.7])
+    columns = ["qc1", "qc2", "qc3", "qc4", "min", "max", "mean", "std", "snr", "num_peaks",  "defect"]
+    df = df[columns]
+    return df.drop(columns = ["defect"]), df["defect"], pixelmap, columns
