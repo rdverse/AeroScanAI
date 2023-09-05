4b70e06e5af4a05f87f7ac4b7824c71d5bd0f88f
rdverse <deveshreddy27@gmail.com>
2023-09-04 22:42:57 -0500

    New updates in serve function for train

--- defect_classify/src/model.py
+++ defect_classify/src/model.py
@@ -7,13 +7,15 @@ class PredictionPayload(BaseModel):
     scaler: bool = True
     
 class TrainPayload(BaseModel):
-    file: str
+    img_dim: int
+    n_channels: int
+    test_scan: str 
+    train_scan: str 
     model_name: str
     model_path: str
-    test_size: int = 25  
-    ncpu: int = 4 
+    append_path: str
+    ncpu: int = 1
     
 class AppendDataPayload(BaseModel): 
     data_path: str
     data: list
-    
