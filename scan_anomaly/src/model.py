254b2b92e4fc30fb7b50f01b867b380044cd9977
rdverse <deveshreddy27@gmail.com>
2023-09-10 17:05:57 -0500

    model param update

--- scan_anomaly/src/model.py
+++ scan_anomaly/src/model.py
@@ -1,41 +1,24 @@
 from pydantic import BaseModel
 
 class PredictionPayload(BaseModel):
-    # x_coord: int
-    # y_coord: int
-    # model_path: str
-    # data_path: str
-    # num_class: int = 2
-    img_dim : int
-    n_channels : int
-    test_scan : str
     model_name : str
     model_path: str
-    x_coord: int
-    y_coord: int
-    num_class: int = 2
-    scaler: bool = False    
-    # remove scaler since random forest won't have much influence of scaling features
+    img_dim : int
+    n_channels : int
+    n_classes : int = 1
+    test_scan : str
+    num_class: int = 1 
     
 class TrainPayload(BaseModel):
     img_dim: int
     n_channels: int
     n_classes: int = 1
     n_samples: int
-    test_scan: str 
     model_name: str
     model_path: str
-    append_path: str
     n_cpus: int = 8
     n_epochs: int = 10
     batch_size: int = 32
     percent_test: float = 0.2
     
 
-class FetchCoordinatesPayload(BaseModel):
-    img_dim : int
-    n_channels : int
-    test_scan : str
-    x_coord : int
-    y_coord : int
-    
