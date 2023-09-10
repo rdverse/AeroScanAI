401d7bd41817815c07da95d3717cf11948cd0a09
rdverse <deveshreddy27@gmail.com>
2023-09-09 19:23:23 -0500

    some rework in phase 2

--- app_ui/pages/Phase[2] Waveform Probing.py
+++ app_ui/pages/Phase[2] Waveform Probing.py
@@ -1,7 +1,8 @@
 import streamlit as st
 from PIL import Image
 import requests
-
+import pandas as pd
+import numpy as np
 # Set the title of the app
 st.title('Waveform Probing')
 
@@ -13,38 +14,60 @@ with app_tab:
     # Header image
     col11, col22 = st.columns(2)
     with col11:
-        #image = Image.open('./assets/qaqc.png')
-        #st.image(image)
-        st.markdown("text")
+        image = Image.open('./assets/waveform2.png')
+        st.image(image)
+        st.markdown("""
+                    ###### Phase 2 : 
+                    """)
     with col22:
-        st.markdown(
-            """
-            ##### Detect defect in fuselages
-            """
-        )
+        st.markdown("""
+            ##### Phase 2 : Analyze waveform of each pixel in a 3D scan. 
+            Specifically, train a random forest model on with raw waveform data and 
+            use explainable-AI for inference and learn what parts of waveform were given most importance.   
+            """)
+        
+    cola1, cola2 = st.columns(2)
+    with cola1:
+        test_scan = st.selectbox('Test Scan', ["low_defect_scan", "medium_defect_scan","high_defect_scan", "random"], placeholder="low_defect_scan")
+        train_scan = st.selectbox('Train Scan', ["low_defect_scan", "medium_defect_scan","high_defect_scan", "random"], placeholder="high_defect_scan")
+        append_path = st.text_input('Training Append Data File Path (If you appended new data below)', key='data', value='') 
+        model_path = st.text_input('Model Save Path', key='model path', value='./box/models/waveform_probe/')
+        
+    with cola2:    
+        n_channels = st.slider('Number of channels in waveform',min_value=1, max_value=512, value=10, step=1)
+        img_dim = st.selectbox('Image Dimension', [8,16,32,"64",128,256,512], placeholder="64")
+        model_name = st.text_input('Model Name',key='model name', help='The name of the model (change when re-training)', value='model')
+        with st.expander("More info on data"):
+            st.info("""
+                   ###### Recomended : Choose test_scan and train_scan with low, medium and high defect scans.
+                   ######              These scans are persisted through the three phases, so you can choose the same scans for all phases.
+                   ######             (It is advised to choose high_defect_scan for training and others for testing (due to data imbalance in low/medium defect scans)")
+                   ###### Choose the following data file if you loaded csv prior to running docker ./box/datasets/defect_classify/train.csv
+                    """)
     # Separator
     st.divider()
     st.markdown(
         """
-        #### Aero defect classification model training
-        """
-    )
+        #### Waveform model training
+        """    )
     # Input data
-    data_file = st.text_input('Training Data File Path', key='data', value='./box/datasets/waveform_probe/train.csv')
-    
-    model_path = st.text_input('Model Save Path', key='model path', value='./box/models/waveform_probe/')
-    model_name = st.text_input('Model Name',key='model name', help='The name of the model without extensions', value='model')
-    test_size = st.slider('Percentage of data saved for Testing',min_value=5, max_value=50, value=25, step=5)
+    # data_file = st.text_input('Training Data File Path', key='data', value='./box/datasets/waveform_probe/train.csv')
+    # model_path = st.text_input('Model Save Path', key='model path', value='./box/models/waveform_probe/')
+    # model_name = st.text_input('Model Name',key='model name', help='The name of the model without extensions', value='model')
+    # test_size = st.slider('Percentage of data saved for Testing',min_value=5, max_value=50, value=25, step=5)
 
     # Button to train the model
     if st.button('Train Model', key='training'):
         # Build the request
         URL = 'http://waveform_probe:5002/train'
     
-        DATA = {'file':data_file, 
-                'model_name':model_name, 
+        DATA = {  'img_dim':img_dim, 
+                'n_channels':n_channels,
+                'test_scan':test_scan,
+                'train_scan':train_scan,
+                'model_name':model_name, # 'model
                 'model_path':model_path, 
-                'test_size': test_size, 
+                'append_path' : append_path,
                 'ncpu': 1}
         print(DATA)
         TRAINING_RESPONSE = requests.post(url=URL, json=DATA)
@@ -63,14 +86,12 @@ with app_tab:
     st.divider()
     st.markdown(
         """
-        #### Aero defect classification analysis
+        #### Waveform defect prediction and interpretation using explainable-AI.
         """
     )
 
-    st.markdown('#### Predictive Maintenance Analysis')
-    
-    selected_model_path = st.text_input('Selected Model Path',key='model path selection', value='./box/models/waveform_probe/model.joblib')
-    selected_data_path = st.text_input('Selected Data Path',key='data path selection', value='')
+    model_name = st.text_input('Selected Model Name',key='model name selection', value='model')
+    model_path = st.text_input('Selected Model Path',key='model path selection', value='./box/models/waveform_probe/')
     col21, col22, col23 = st.columns(3)
 
     with col21:
@@ -82,19 +103,39 @@ with app_tab:
     #sample = [{'x_coord':10, 'y_coord':10}]
     sample = [{'x_coord':x_coord, 'y_coord':y_coord}]
     
-    if st.button('Run Maintenance Analysis', key='analysis'):
+    if st.button('Analyze waveform', key='analysis'):
         URL = 'http://waveform_probe:5002/predict'
-        DATA = {'model_path':selected_model_path, 'data_path' : selected_data_path, 'x_coord':x_coord , 'y_coord' : y_coord ,'num_class':3}
+        DATA = {'model_name': model_name, 
+                'model_path':model_path,
+                'n_channels': n_channels, 
+                'img_dim': img_dim, 
+                'test_scan' : test_scan, 
+                'x_coord':x_coord , 
+                'y_coord' : y_coord ,
+                'num_class':2}
         INFERENCE_RESPONSE = requests.post(url = URL, json = DATA)
         #print(INFERENCE_RESPONSE.json())
         st.info(INFERENCE_RESPONSE.text)
+        ####################uncomment
         import ast
         st.info(type(INFERENCE_RESPONSE.json().get('wavetoplot')))
         data = ast.literal_eval(INFERENCE_RESPONSE.json().get('wavetoplot'))
         feature_importance = ast.literal_eval(INFERENCE_RESPONSE.json().get('feature_importance'))
-        st.info(data)
-        st.line_chart(data)
-        st.bar_chart(feature_importance)
+        data, feature_importance, time = np.array(data).reshape(-1,1), np.array(feature_importance).reshape(-1,1), np.arange(len(data)).reshape(-1,1)
+        dataImpDF = pd.DataFrame(np.hstack((data, feature_importance, time)), columns = ['Amplitude', 'Feature Importance', 'Time'])
+        
+        st.line_chart(dataImpDF, 
+                      y = 'Amplitude',
+                      x = 'Time')
+        st.bar_chart(dataImpDF, 
+                     y = 'Feature Importance',
+                     x = 'Time')
+        with st.expander('More info on feature importance'):
+            st.info("""
+                    ###### Feature Importance
+                    Here we use LIME (local agnostic model explanation) to explain the model's prediction for a given input (i.e., the raw waveform).
+                    """)
+       ################## 
         # print(INFERENCE_RESPONSE)
         # st.info(INFERENCE_RESPONSE)
         # st.info(INFERENCE_RESPONSE.text)
@@ -107,8 +148,12 @@ with app_tab:
         #     #st.line_chart()
     
 with help_tab:
-    st.markdown("#### Input Descriptions for Ultrasonic NDT Testing:")
+    st.markdown("#### Importance of Waveform Probing")
     st.markdown(        """
-        #### Help
-        This app is still under development. Please contact the github repo owner for more details."""
-    )
+        #### Why is it important to analyze the waveform of each pixel in a 3D scan?
+        #####  The waveform of each pixel in a 3D scan has immense information about the defect or a normal signal. 
+        ##### Therefore, we first train an random forest model and pick the optimal configuration using grid search.
+        ##### Next, we use explainable-AI such as LIME to interpret the model's prediction for a given input (i.e., the raw waveform).
+        ##### This helps us understand what parts of the waveform were given most importance for the prediction.
+        ##### To interpret this plot, .
+    """)
